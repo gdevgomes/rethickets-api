b81938a45df400ab5c11be09c3355eee2a235983
Filipe Prado - Rethink <filipe.prado@rethink.dev>
2022-03-17 11:59:40 -0300

    chore: moving config of knex to database folder

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,30 +1,35 @@
-import bcrypt from 'bcrypt';
-import knexConfig from "../config/knexConfig";
+import bcrypt from "bcrypt";
+import knex from "../database";
 
-const jwt = require("jsonwebtoken")
-const knex = require("knex")(knexConfig);
+const jwt = require("jsonwebtoken");
 
 const authentication = async ({ username, password }) => {
-    try {
-        const user = await knex.select('password', 'username', 'id').from('users').where({ username }).first()
+  try {
+    const user = await knex
+      .select("password", "username", "id")
+      .from("users")
+      .where({ username })
+      .first();
 
-        const verify = await bcrypt.compare(password, user.password);
+    const verify = await bcrypt.compare(password, user.password);
 
-        if (!verify) throw new Error("Wrong Password");
+    if (!verify) throw new Error("Wrong Password");
 
-        const data = {
-            first_name, last_name, email,
-            token: jwt.sign(
-                {
-                    user_id: id,
-                    exp: Date.now() / 1000 + 86400,
-                },
-                process.env.TOKEN_SECRET
-            ),
-        }
-    } catch (error) {
-        console.log(error)
-    }
+    const data = {
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      token: jwt.sign(
+        {
+          user_id: user.id,
+          exp: Date.now() / 1000 + 86400,
+        },
+        process.env.TOKEN_SECRET
+      ),
+    };
+  } catch (error) {
+    console.log(error);
+  }
 
-    return
-}
+  return;
+};
---
+++ src/database/index.ts
@@ -0,0 +1,4 @@
+import knexConfig from "../../knexfile";
+const knex = require("knex")(knexConfig);
+
+export default knex;
